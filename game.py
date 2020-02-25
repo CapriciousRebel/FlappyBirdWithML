9b9004249ea1106e14f75e49737950b741ca963c
Smith <codingfreak98@Smiths-MacBook-Pro.local>
2020-02-26 03:47:02 +0530

    wrap up

--- game.py
+++ game.py
@@ -210,14 +210,18 @@ class Base:
         win.blit(self.image, (self.x2, self.y))
 
 
-def draw_window(win, bird, pipes, base, score, collideText):
+def draw_window(win, birds, pipes, base, score, collideText):
     win.blit(background_sprite, (0, 0))
 
     for pipe in pipes:
         pipe.draw(win)
-    base.draw(win)
-    bird.draw(win)
 
+    for bird in birds:
+        bird.draw(win)
+
+    base.draw(win)
+
+    
     text = STAT_FONT.render("Score : " + str(score), 1, (255, 255, 255))
     win.blit(text, (WINDOW_WIDTH - 10 - text.get_width(), 10))
 
@@ -228,7 +232,20 @@ def draw_window(win, bird, pipes, base, score, collideText):
 
 
 def main(genomes, config):
-    birds = Bird(230, 350)
+
+    nets = []
+    ge = []
+    birds = []
+
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+
+        birds.append(Bird(230, 350))
+
+        g.fitness = 0
+        ge.append(g)
+
     base = Base(730)
     pipes = [Pipe(600)]
 
@@ -247,47 +264,73 @@ def main(genomes, config):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
+                quit()
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    bird.jump()
+#            if event.type == pygame.KEYDOWN:
+#               if event.key == pygame.K_SPACE:
+#                   bird.jump()
 
-        bird.move()
+        pipe_ind = 0
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].top_pipe.get_width():
+                pipe_ind = 1
+        else:
+            run = False
+            break
+
+        for x, bird in enumerate(birds):
+            bird.move()
+            ge[x].fitness += 0.1
+
+            output = nets[x].activate((bird.y, abs(
+                bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
+
+            if output[0] > 0.5:
+                bird.jump()
 
         #### Pipe genration logic ####
         add_pipe = False
         remove_pipes = []
         for pipe in pipes:
-            if pipe.collide(bird):
-                collideText = "Pipe collide"
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    # If a bird collides with a pipe, then it must lose some of its fitness so that it is not favoured as much as a bird that didnt hit the pipe at the same level
+                    ge[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
 
             if pipe.x + pipe.top_pipe.get_width() < 0:
                 remove_pipes.append(pipe)
 
-            if not pipe.passed and pipe.x < bird.x:
-                pipe.passed = True
-                add_pipe = True
-
             pipe.move()
+
         if add_pipe:
             score += 1
+            # When a bird crosses a pipe, it gets a bonus fitness of +5 as an encouragement for it to pass through the pipes
+            # Here we dont need to loop through all the birds since only those birds who make it through the pipes will be alive in the genome
+            for g in ge:
+                g.fitness += 5
+
             pipes.append(Pipe(600))
 
         for remove_pipe in remove_pipes:
             pipes.remove(remove_pipe)
 
         # bottom collision
-        if bird.y + bird.image.get_height() >= 730:
-            collideText = "bottom collide!"
+        for x, bird in enumerate(birds):
+            if bird.y + bird.image.get_height() >= 730 or bird.y < 0:
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
 
         base.move()
-        draw_window(win, bird, pipes, base, score, collideText)
-
-    pygame.quit()
-    quit()
-
-
-main()
+        draw_window(win, birds, pipes, base, score, collideText)
 
 
 def run(config_path):
